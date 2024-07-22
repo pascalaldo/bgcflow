4c65c34d0bbbbc9e8462abe2c82dbc90ba45b0ef
Pascal A. Pieters <paspie@biosustain.dtu.dk>
2024-07-22 09:02:44 +0000

    PanKB alleleome upload script

--- workflow/scripts/upload_static_to_pankb.sh
+++ workflow/scripts/upload_static_to_pankb.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 for d in data/processed/*/ ; do
-    echo "$d"
+    SPECIES=`basename $d`
+    ALLELEOME_DIR="${d}alleleome/"
+    echo $SPECIES
+    
+    for f in dn_ds.json final_dn_ds_count_per_gene.csv step_line.json ; do
+        azcopy copy --overwrite=false ${ALLELEOME_DIR}Pan/$f https://pankb.blob.core.windows.net/data/PanKB/web_data/species/$SPECIES/panalleleome/$PANKB_WRITE_SAS
+    done
+    azcopy copy --overwrite=false --recursive ${ALLELEOME_DIR}gene_data https://pankb.blob.core.windows.net/data/PanKB/web_data/species/$SPECIES/panalleleome/$PANKB_WRITE_SAS
 done
