edf1e2b55d3c9a5d2dca54c758d58e867ac18d37
Daniel C. Romero Yianni <danyia@dtu.dk>
2025-02-21 13:47:09 +0100

    better triangle matrix to full matrix conversion

--- workflow/bgcflow/bgcflow/data/convert_triangular_matrix.py
+++ workflow/bgcflow/bgcflow/data/convert_triangular_matrix.py
@@ -1,40 +1,46 @@
+"""
+Convert a triangular matrix (missing everything above the diagonal, such as those output by mash) to a complete, 
+symmetrical matrix
+"""
+import csv
 import logging
-import sys
-
-import numpy as np
-import pandas as pd
-
-log_format = "%(levelname)-8s %(asctime)s   %(message)s"
-date_format = "%d/%m %H:%M:%S"
-logging.basicConfig(format=log_format, datefmt=date_format, level=logging.DEBUG)
-
-
-def triangle_convert(matrix, outfile):
-    df_raw = pd.read_csv(matrix, index_col=0)
-    genome_id_list = []
-    for idx in df_raw.index:
-        genome_id = idx.split("\t")[0].split("/")[-1].split(".fna")[0]
-        genome_id_list.append(genome_id)
-
-    df = pd.DataFrame(0, index=genome_id_list, columns=genome_id_list)
-    for idx in df_raw.index:
-        genome_id = idx.split("\t")[0].split("/")[-1].split(".fna")[0]
-        for cntr in range(len(idx.split("\t"))):
-            if cntr > 0:
-                value = idx.split("\t")[cntr]
-
-                # if value is NA, replace with numpy null
-                if value == "NA":
-                    value = np.nan
-                else:
-                    value = float(value)
-
-                df.loc[genome_id, genome_id_list[cntr - 1]] = value
-                df.loc[genome_id_list[cntr - 1], genome_id] = value
-    df.index.name = "genome_id"
-    df.to_csv(outfile)
-    return
-
+from itertools import chain
+from os.path import basename, splitext
+from sys import argv
 
 if __name__ == "__main__":
-    triangle_convert(sys.argv[1], sys.argv[2])
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s - %(levelname)s] - %(message)s")
+    logger = logging.getLogger()
+
+    triangle_mtx_fp, full_matrix_out_fp = argv[1], argv[2]
+
+    genome_names = []
+
+    with open(triangle_mtx_fp) as file:
+        for i, line in enumerate(file):
+            if i == 0:
+                n_genomes = line.strip()
+                assert n_genomes.isnumeric(), f"First line should be an integer, got '{n_genomes}'"
+                n_genomes = int(n_genomes)
+                full_matrix = [[0] * n_genomes for _ in range(n_genomes)]
+                continue
+
+            row_values = line.split("\t")
+
+            genome_path = row_values.pop(0)
+            genome_file = basename(genome_path)
+            genome_name, _ = splitext(genome_file)
+            genome_names.append(genome_name)
+
+            row_distances = [float(val) for val in row_values]
+
+            for j, distance in enumerate(row_distances):
+                full_matrix[i - 1][j] = distance
+                full_matrix[j][i - 1] = distance
+
+    with open(full_matrix_out_fp, "w") as outfile:
+        csv_writer = csv.writer(outfile)
+        csv_writer.writerow(chain(["genome_id"], genome_names))
+        for i, row in enumerate(full_matrix):
+            genome_name = genome_names[i]
+            csv_writer.writerow(chain([genome_name], row))
